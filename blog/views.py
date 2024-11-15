ccb186856ecd18dcbecd17865be6cc41dc1de565
YashwanthK <yashwanthkothakota@gmail.com>
2024-11-15 15:23:46 +0530

    add seo friendly url

--- blog/views.py
+++ blog/views.py
@@ -4,12 +4,19 @@ from .models import Post
 from django.http import Http404
 
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     # try:
     #     post = Post.published.get(id=id)
     # except Post.DoesNotExist:
     #     raise Http404("No Post found")
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
+    )
     return render(request, 'blog/post/detail.html', {'post': post})
 
 
