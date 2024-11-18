bfa38b78e03e0c85914770e4324e54735d3199d1
YashwanthK <yashwanthkothakota@gmail.com>
2024-11-18 11:32:53 +0530

    add similar posts fetch

--- blog/views.py
+++ blog/views.py
@@ -13,6 +13,7 @@ from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
 
 
 @require_POST
@@ -111,7 +112,20 @@ def post_detail(request, year, month, day, post):
     )
     comments = post.comments.filter(active=True)
     form = CommentForm()
-    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids
+    ).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(
+        same_tags=Count('tags')
+    ).order_by('-same_tags', '-publish')[:4]
+    return render(
+        request,
+        'blog/post/detail.html',
+        {'post': post, 'comments': comments,
+            'form': form, 'similar_posts': similar_posts}
+    )
 
 
 def post_list(request, tag_slug=None):
